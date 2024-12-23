3238ce015c747f50cfdf311548b2d8e65cf35a7c
Shento Hendriks <shento@studioshento.nl>
2024-12-23 01:02:25 +0100

    Started the basic landpage for the site

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <>
-      <h1>Prosifier</h1>
-    </>
+    <main className="container">
+      <h1 className="text-6xl text-center max-w-[690px] m-auto font-medium mb-5 leading-tighter tracking-tight">
+        <span className="text-blue-500">Prosifier</span>, the modern writing tool built by authors, for authors.
+      </h1>
+      <p className="text-center max-w-[20rem]  mx-auto">Outline, draft, edit, and refine—all in one intuitive platform. Also it’s completely free.</p>
+    </main>
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      container: {
+        center: true,
+        padding: "1em",
       },
     },
   },
